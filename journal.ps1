c368ae049fb927b2f1bd0a9ccd3a0f82880c42a3
Wogan May <wogan.may@gmail.com>
2019-10-10 11:37:28 +0200

    Create journal.ps1

---
+++ journal.ps1
@@ -0,0 +1,23 @@
+#
+# pkb1000/journal
+# Open today's journal file. Copies in a starter template if one exists.
+#
+
+$datePath = $PSScriptRoot + "\" + (Get-Date).toString("yyyy\\MM\\dd")
+$journalFilePath = $datePath + "\journal.txt"
+$journalTemplatePath = $PSScriptRoot + "\setup\journal.txt"
+
+if (-not (test-path $datePath)) {
+    new-item -path $datePath -ItemType Directory
+}
+
+if (-not (test-path $journalFilePath)) {
+    if ((test-path $journalTemplatePath)) {
+        Copy-Item $journalTemplatePath $journalFilePath
+    } else {
+        New-Item $journalFilePath
+        Set-Content $journalFilePath "Set up a template at $($journalTemplatePath) for more interesting starter content."
+    }
+}
+
+notepad $journalFilePath
