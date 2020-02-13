4d845182ac699e3e569fec0dccc94a13786181fa
Kobus <smikobus@gmail.com>
2020-02-13 19:21:29 +0200

    change folder structure
    Change the created folder structure to monthly creating files with day in file name. Code will open the month's directory
    Change location of template/setup file
    
    Update readme file to include template information

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # pkb1000
 
-Inspired by @cvoxel on Twitter - these scripts make it easy to create a daily journal file in a year/month/day folder structure. Store them inside a Dropbox-synced folder to go cross-platform.
+Inspired by @cvoxel on Twitter - these scripts make it easy to create a daily journal file in a year/month folder structure. Store them inside a Dropbox-synced folder to go cross-platform.
+
+Create a template.md file in the base directory to add some structure
+
+
+uses vscode as the default editor to make markdown files
--- journal.ps1
+++ journal.ps1
@@ -3,9 +3,9 @@
 # Open today's journal file. Copies in a starter template if one exists.
 #
 
-$datePath = $PSScriptRoot + "\" + (Get-Date).toString("yyyy\\MM\\dd")
-$journalFilePath = $datePath + "\journal.md"
-$journalTemplatePath = $PSScriptRoot + "\setup\journal.md"
+$datePath = $PSScriptRoot + "\" + (Get-Date).toString("yyyy\\MM")
+$journalFilePath = $datePath + "\journal_" + (Get-Date).toString("dd") + ".md"
+$journalTemplatePath = $PSScriptRoot + "\template.md"
 
 if (-not (test-path $datePath)) {
     new-item -path $datePath -ItemType Directory
@@ -14,10 +14,11 @@ if (-not (test-path $datePath)) {
 if (-not (test-path $journalFilePath)) {
     if ((test-path $journalTemplatePath)) {
         Copy-Item $journalTemplatePath $journalFilePath
-    } else {
+    }
+    else {
         New-Item $journalFilePath
         Set-Content $journalFilePath "Set up a template at $($journalTemplatePath) for more interesting starter content."
     }
 }
 
-code $journalFilePath
+code $datePath
